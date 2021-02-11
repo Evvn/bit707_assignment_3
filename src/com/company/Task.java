8ea4232ff10058390770a014f169f7107a9472f4
Evan <e.amezcua@damstratechnology.com>
2021-02-11 19:10:08 +1100

    created task class skeleton

--- src/com/company/Task.java
+++ src/com/company/Task.java
@@ -1,4 +1,24 @@
 package com.company;
 
+import java.util.Date;
+
 public class Task {
+
+    // task attribute fields
+    private int taskId;
+    private String taskTitle;
+    private String taskDescription;
+    private boolean isComplete;
+    private Date creationDate;
+    private Date updatedDate;
+    private Date dueDate;
+
+    // task methods
+    public void updateTask(String newTitle, String newDescription, Date newDueDate) {
+        // update task here
+    }
+
+    public void completeTask(boolean isComplete) {
+        // set task to complete or not here
+    }
 }
